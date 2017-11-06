1b0174a25258f14a6d7e2774e89b129689f7c075
Crista Cooper <aine3d@gmail.com>
2017-11-06 03:14:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ class TicTacToe
       input = gets.chomp.to_i
       index = input_to_index(input)
       if valid_move?(index)
-        move(index, current_player(board))
+        move(index, current_player)
         display_board
       else
         turn
