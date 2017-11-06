c84fd0731f9dc84c14a31300a0eb8c9c24a07f21
Crista Cooper <aine3d@gmail.com>
2017-11-06 03:12:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,7 +68,7 @@ class TicTacToe
       @board.count{|token| token == "X" || token == "O"}
     end
 
-    def current_player(board)
+    def current_player
       turn_count % 2 == 0 ? "X" : "O"
     end
 
